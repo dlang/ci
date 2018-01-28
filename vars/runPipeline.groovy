c1519f433985d387fd946bbbe0c2e1c5b3d9511e
Jacob Carlborg <doob@me.com>
2018-01-28 10:19:41 +0100

    Fix indentation

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -174,7 +174,7 @@ def testDownstreamProject (name) {
             try { dir(name) {
 
                 if (repo == 'vibe-d/vibe.d') {
-					 clone("https://github.com/${repo}.git", 'v0.8.3-alpha.3')
+                    clone("https://github.com/${repo}.git", 'v0.8.3-alpha.3')
                 } else if (repo == "sociomantic-tsunami/ocean") {
                     clone("https://github.com/${repo}.git", 'v4.0.0-alpha.5')
                 } else {
