84fb68511318340dd81fc6184bdb6f7a8ef895a6
Sebastian Wilzbach <seb@wilzba.ch>
2018-07-31 00:57:09 +0200

    Install the llvm package

--- buildkite.sh
+++ buildkite.sh
@@ -50,6 +50,7 @@ projects=(
     "vibe-d/vibe-core+select" # 3m30s
     "higgsjs/Higgs" # 3m10s
     "rejectedsoftware/ddox" # 2m42s
+    "ldc-developers/ldc" # 2m15s
     "BlackEdder/ggplotd" # 1m56s
     "eBay/tsv-utils" # 1m41s
     "dlang-community/D-Scanner" # 1m40s
@@ -69,7 +70,6 @@ projects=(
     "dlang-community/libdparse" # 13s
     "BBasile/iz" # 12s
     "dlang-community/dfmt" # 11s
-    "ldc-developers/ldc" # TODO
     # run in under 10s sorted alphabetically
     "Abscissa/libInputVisitor"
     "ariovistus/pyd"
