9409e7d4a9f16931fec36e6dc12db6028eddec31
Sebastian Wilzbach <seb@wilzba.ch>
2018-07-30 19:01:38 +0200

    Add LDC to the project tester

--- buildkite.sh
+++ buildkite.sh
@@ -69,6 +69,7 @@ projects=(
     "dlang-community/libdparse" # 13s
     "BBasile/iz" # 12s
     "dlang-community/dfmt" # 11s
+    "ldc-developers/ldc" # TODO
     # run in under 10s sorted alphabetically
     "Abscissa/libInputVisitor"
     "ariovistus/pyd"
