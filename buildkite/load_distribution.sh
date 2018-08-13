25def9937dd9d6f38e48356c083505f34cd3af3b
Sebastian Wilzbach <seb@wilzba.ch>
2018-08-13 15:44:49 +0200

    Load distribution for the style target too

---
+++ buildkite/load_distribution.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PS4="~> " # needed to avoid accidentally generating collapsed output
+set -uexo pipefail
+
+echo "--- Setting build variables"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export PATH="$PWD/distribution/bin:${PATH:-}"
+# set HOME to separate concurrent ~/.dub user paths
+export HOME="$PWD"
+export LIBRARY_PATH="$PWD/distribution/libs:${LIBRARY_PATH:-}"
+export LD_LIBRARY_PATH="$PWD/distribution/libs:${LD_LIBRARY_PATH:-}"
+export DETERMINISTIC_HINT=1
+
+# Make sure there are no overlaps of the build files by putting them in the current directory
+rm -rf tmp && mkdir tmp
+export TMP=$PWD/tmp
+export TMPDIR=$PWD/tmp
+export TEMP=$PWD/tmp
+
+export DC=dmd
+export DMD=dmd
