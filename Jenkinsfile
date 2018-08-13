5ec7adfd9b39286f481d4c78e99f8b989a54547d
Sebastian Wilzbach <seb@wilzba.ch>
2018-08-13 15:55:30 +0200

    Remove Jenkin files

--- Jenkinsfile
+++
@@ -1,5 +0,0 @@
-#!/bin/env groovy
-// use legacy checkout SCM to fetch library, so we test changes froms PRs/branches
-// https://github.com/jenkinsci/workflow-cps-global-lib-plugin/pull/37#issuecomment-311608135
-library identifier: "dlang@master", retriever: legacySCM(scm)
-runPipeline()
--- ansible/roles/jenkins/files/backup.groovy
+++
@@ -1,5 +0,0 @@
-import org.jvnet.hudson.plugins.thinbackup.ThinBackupPluginImpl;
-import org.jvnet.hudson.plugins.thinbackup.ThinBackupPeriodicWork.BackupType;
-import org.jvnet.hudson.plugins.thinbackup.backup.HudsonBackup;
-
-new HudsonBackup(ThinBackupPluginImpl.getInstance(), BackupType.FULL).backup()
