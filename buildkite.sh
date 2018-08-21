96a9baf33d04a24a58811ae87a3f18576ce7645e
Sebastian Wilzbach <seb@wilzba.ch>
2018-08-21 17:28:20 +0200

    Run DMD's testsuite on Buildkite

--- buildkite.sh
+++ buildkite.sh
@@ -143,6 +143,7 @@ projects=(
     "BBasile/iz" # 12s
     "aliak00/optional" # 12s
     "dlang-community/dfmt" # 11s
+    "dlang/dmd" # TODO
     # run in under 10s sorted alphabetically
     "Abscissa/libInputVisitor"
     #"ariovistus/pyd"
