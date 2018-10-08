751b0aa2938beb049567367aec0572ff1aa6b62e
Martin Nowak <code@dawg.eu>
2018-10-08 18:04:54 +0200

    gen image for buildkite based (nightly) release-builder
    - builds releases for all OSes using virtualbox
    - uploads tarballs to S3
    - updates downloads.dlang.org/nightlies index pages

--- buildkite/origin_target_branch.sh
+++ buildkite/origin_target_branch.sh
@@ -2,21 +2,21 @@
 
 # Returns the origin target branch to clone
 
-origin_target_branch="${BUILDKITE_PULL_REQUEST_BASE_BRANCH:-$BUILDKITE_BRANCH}"
-
 repo="$1"
-
 if [ -e "$repo" ] ; then
     (>&2 echo "Did you forget to pass in a repository?")
     exit 1
 fi
-
-branch=master
+if [ $# -eq 2 ]; then
+    origin_target_branch="$2"
+else
+    origin_target_branch="${BUILDKITE_PULL_REQUEST_BASE_BRANCH:-$BUILDKITE_BRANCH}"
+fi
 
 if [ "$origin_target_branch" == "master" ] || [ "$origin_target_branch" == "stable" ] ; then
     branch="$origin_target_branch"
 else
-    # check whether the current branch exists on the to be checked out repo
-    branch=$(git ls-remote --exit-code --heads "$repo" "${origin_target_branch}" > /dev/null && echo "$origin_target_branch" || echo "master")
+    # check whether the target branch exists for the given repo or fallback to master if it doesn't
+    branch=$(git ls-remote --exit-code --heads "$repo" "$origin_target_branch" > /dev/null && echo "$origin_target_branch" || echo 'master')
 fi
 echo "$branch"
