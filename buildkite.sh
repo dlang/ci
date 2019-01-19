f00c851e945068bffcb9796c2372982052987487
Basile Burg <basile.b@gmx.com>
2019-01-19 18:28:59 +0100

    update links for the project "iz"

--- buildkite.sh
+++ buildkite.sh
@@ -146,7 +146,7 @@ projects=(
     "dlang-community/D-YAML" # 15s
     "libmir/mir-random" # 13s
     "dlang-community/libdparse" # 13s
-    "BBasile/iz" # 12s
+    "Basile-z/iz" # 12s
     "aliak00/optional" # 12s
     "dlang-community/dfmt" # 11s
     # run in under 10s sorted alphabetically
@@ -180,7 +180,7 @@ projects=(
 # Add all projects that require more than 3GB of memory to build
 declare -A memory_req
 memory_req["BlackEdder/ggplotd"]=high
-memory_req["BBasile/iz"]=high
+memory_req["Basile-z/iz"]=high
 memory_req["dlang-community/D-Scanner"]=high
 memory_req["vibe-d/vibe-core+select"]=high
 memory_req["vibe-d/vibe-core+epoll"]=high
