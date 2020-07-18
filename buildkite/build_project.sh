cfa7683ece6762b4a566d0dde9013f640bd31dbb
Martin Kinkelin <kinke@users.noreply.github.com>
2020-07-19 00:36:06 +0200

    try with clang + lld

--- buildkite/build_project.sh
+++ buildkite/build_project.sh
@@ -231,11 +231,13 @@ case "$REPO_FULL_NAME" in
     ldc-developers/ldc)
         git submodule update --init
         mkdir bootstrap && cd bootstrap
+        export CC=clang-8
+        export CXX=clang++-8
         cmake .. \
           -GNinja \
           -DCMAKE_BUILD_TYPE=Debug \
           -DD_COMPILER="$DC" \
-          -DCMAKE_EXE_LINKER_FLAGS=-fuse-ld=gold # gold required for llvm-8 pkg
+          -DCMAKE_EXE_LINKER_FLAGS=-fuse-ld=lld # lld required for llvm-8 pkg
         ninja -j2 ldmd2 druntime-ldc-debug phobos2-ldc-debug
         cd ..
         mkdir build && cd build
